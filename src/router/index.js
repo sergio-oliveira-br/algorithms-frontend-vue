5b6c47b536d5f51e9a1702e48e77da168621c566
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-03 14:57:46 +0100

    Update index.js
    act:
    -> view imported and routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import SortingAlgorithmView from "../views/SortingAlgorithmView.vue";
 
 
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
         path: '/',
         name: 'home',
         component: HomeView
+    },
+    {
+        path: '/algorithms/sorting-algorithm',
+        name: 'algorithms',
+        component: SortingAlgorithmView
     },
 ];
 
