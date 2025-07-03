c44976315fbcd89ee6e3e8817ca0458ba8b1075b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-03 13:10:00 +0100

    Create HomeView.vue
    act:
    -> home view created

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center p-8 bg-white rounded-lg shadow-lg">
+    <h1 class="text-4xl font-bold text-gray-800 mb-4">Welcome to the Algorithm Explorer!</h1>
+    <p class="text-lg text-gray-600">Please select an algorithm from the side menu to start.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
