b8dd86d99ec7d96234a38fb84089194a67c3b5e7
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-03 14:57:23 +0100

    Create SortingAlgorithmView.vue
    act:
    -> view created
    this view will be responsible to display functionalities from Sort Algorithms

---
+++ src/views/SortingAlgorithmView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-start p-8 bg-white rounded-lg shadow-lg w-full max-w-lg">
+    <h1>Sort Algorithm</h1>
+    <p>Please enter the numbers to order and see result</p>
+
+    <div>
+      <label></label>
+      <input>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
