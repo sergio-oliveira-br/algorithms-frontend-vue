acf0f95e33c6a45e21b14aca8ecc7731ec93fba3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-04 14:39:36 +0100

    Update SortingAlgorithmView.vue
    act:
    -> script to fetch the Random Number Generator

--- src/views/SortingAlgorithmView.vue
+++ src/views/SortingAlgorithmView.vue
@@ -1,5 +1,43 @@
 <script setup>
+  import {ref} from 'vue';
+  import {useApiFetch} from '../composables/useApiFetch.js';
 
+  // --- Logic for the Random Number Generator --
+  const numberOfNumbersToGenerate = ref(50);
+  const generatedNumbersArray  = ref([]);
+  const errorMessage = ref('');
+
+  // Renamed variables returned to avoid name conflicts in the component
+  const {
+    data: generatedApiData,
+    loading: isGeneratedApiLoading,
+    errorMsg: generatedApiError,
+    fetchData: callGenerateApi,
+  } = useApiFetch();
+
+  // Function to be called by the clinck "Generate" button
+  const generateNumbers = async () => {
+
+    //cleanup
+    errorMessage.value = '';
+    generatedNumbersArray.value = [];
+
+    if (numberOfNumbersToGenerate.value <= 0 ){
+      generatedApiError.value = 'This number must be greater than 0.'
+    }
+
+    const url = `http://localhost:8080/api/v1/sort/generator?qtyOfNumbersToGenerate=${numberOfNumbersToGenerate.value}`;
+
+    await callGenerateApi(url, {method: 'POST'});
+
+    // Update the variables and components
+    if (generatedApiError.value) {
+      errorMessage.value = generatedApiError.value;
+    }
+    else if (generatedApiData.value) {
+      generatedNumbersArray.value = generatedApiData.value;
+    }
+  };
 </script>
 
 <template>
@@ -11,18 +49,24 @@
       <label class="block text-gray-500">Please enter the number of numbers to be generated.</label>
       <input class="w-full mt-2 p-2 border border-gray-200 rounded-sm
         focus:outline-none focus:ring-2 focus:ring-stone-200"
-          type="number" placeholder="50">
+          type="number" placeholder="50"
+             v-model.number="numberOfNumbersToGenerate">
 
       <button
-          @click=""
+          @click="generateNumbers"
           class="w-full p-2 my-2
             bg-stone-200
               border border-gray-300 rounded-sm
                 text-neutral-500 font-bold
                   shadow-sm hover:shadow-lg
-                    ease-in-out">Generate
+                    ease-in-out">{{ isGeneratedApiLoading ? 'Generating...' : 'Generate Numbers' }}
+        Generate
       </button>
-      <p></p>
+      
+      <div v-if="generatedNumbersArray.length > 0" class="mt-4 p-4 bg-indigo-50 rounded-lg border border-indigo-200">
+        <h4 class="text-lg font-semibold text-indigo-800 mb-2">Generated Numbers:</h4>
+        <p class="font-mono text-indigo-700 break-all text-sm">{{ generatedNumbersArray.join(', ') }}</p>
+      </div>
 
     </div>
   </div>
