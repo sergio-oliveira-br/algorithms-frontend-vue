78e5d303fa69f7e1c0c6fd591410d7232515d9d6
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-04 12:12:23 +0100

    Update SortingAlgorithmView.vue
    act:
    -> card created to get the qty of numbers to generate

--- src/views/SortingAlgorithmView.vue
+++ src/views/SortingAlgorithmView.vue
@@ -4,12 +4,26 @@
 
 <template>
   <div class="flex flex-col items-center justify-start p-8 bg-white rounded-lg shadow-lg w-full max-w-lg">
-    <h1>Sort Algorithm</h1>
-    <p>Please enter the numbers to order and see result</p>
+    <h1 class="font-bold text-3xl mb-3">Sort Algorithm</h1>
+    <h3 class="text-2xl text-gray-800 mb-2">Compare performance of Sort Algoristhm</h3>
 
-    <div>
-      <label></label>
-      <input>
+    <div class="p-4 border border-gray-200 rounded-lg">
+      <label class="block text-gray-500">Please enter the number of numbers to be generated.</label>
+      <input class="w-full mt-2 p-2 border border-gray-200 rounded-sm
+        focus:outline-none focus:ring-2 focus:ring-stone-200"
+          type="number" placeholder="50">
+
+      <button
+          @click=""
+          class="w-full p-2 my-2
+            bg-stone-200
+              border border-gray-300 rounded-sm
+                text-neutral-500 font-bold
+                  shadow-sm hover:shadow-lg
+                    ease-in-out">Generate
+      </button>
+      <p></p>
+
     </div>
   </div>
 </template>
