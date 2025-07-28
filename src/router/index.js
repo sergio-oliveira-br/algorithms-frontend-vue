1a8c16bd31047a0a703ba47595625a8a5550ee3b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-07-28 16:01:55 +0100

    Update index.js
    act:
    -> new route created that refer Linear Search page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import SortingAlgorithmView from "../views/SortingAlgorithmView.vue";
+import LinearSearchMinMax from "@/views/LinearSearchMinMax.vue";
 
 
 const routes = [
@@ -13,6 +14,11 @@ const routes = [
         path: '/algorithms/sorting-algorithm',
         name: 'algorithms',
         component: SortingAlgorithmView
+    },
+    {
+        path: '/linear-search/find-min-max',
+        name: 'linear-search-min-max',
+        component: LinearSearchMinMax
     },
 ];
 
