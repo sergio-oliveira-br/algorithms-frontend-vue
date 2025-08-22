0a977ce42950e31d225d31bf531fcf8a7b1c0700
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2025-08-22 14:20:36 +0100

    Create useFindMinOrMax.ts
    act:
    -> new composable has been created to apply the standards of the good practices of vue. This also makes the code reusable

---
+++ src/composables/useFindMinOrMax.ts
@@ -0,0 +1,75 @@
+// src/composables/useFindMinOrMax.ts
+
+import {ref, watch} from 'vue';
+import { useApiFetch } from "@/composables/useApiFetch";
+
+// Typing the API response
+interface FinderResult {
+    data: number | null;
+    loading: boolean;
+    errorMsg: string | null;
+}
+
+// The composable gets the necessary parameters
+export function useFindMinOrMax() {
+
+    //Typing the composable internal refs
+    const foundValue = ref<number | null>(null);
+    const searchErrorMessage = ref<string | null>(null);
+
+    // Instance of useApiFetch within the composable
+    const {
+        data: searchApiData,
+        loading: isSearchApiLoading,
+        errorMsg: searchApiError,
+        fetchData: callSearchApi,
+    } = useApiFetch<number | null>();
+
+
+    // The main function that will be exposed to the component
+    const findValue = async (array: number[], strategy: 'Min' | 'Max') => {
+
+        // cleanup
+        foundValue.value = null;
+        searchErrorMessage.value = null;
+
+        // validation
+        if (!array || array.length < 1) {
+            searchApiError.value = 'It is not possible to find the min value. ' +
+                '\nThey array must be grater than 2 numbers';
+            return;
+        }
+
+        // build the URL
+        const url = `http://localhost:8080/api/v1/find/${strategy}`
+
+        // make the call
+        await callSearchApi(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify([...array]),
+        });
+
+        // call back is error or success
+        if(searchApiError.value) {
+            searchErrorMessage.value = searchApiError.value;
+        }
+        else if(searchApiData.value !== null) {
+            foundValue.value = searchApiData.value;
+        }
+        else {
+            searchErrorMessage.value = 'Failed to find value: unexpected API response.';
+            console.error('Unexpected find API data:', searchApiData.value);
+        }
+    }
+
+    return {
+        foundValue,
+        searchErrorMessage,
+        isSearchApiLoading,
+        findValue
+    };
+}
+
